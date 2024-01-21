32936b567b28081919028b28e24bf845156b826f
Stylo <136652260+stylo-codes-stuff@users.noreply.github.com>
2024-01-21 21:14:29 +0000

    save commit

--- audioStegTech.py
+++ audioStegTech.py
@@ -1,17 +1,46 @@
-import Audio
-import
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.io.wavfile as wavfile
+
 '''Takes the Least Signidificant Bits of an audio file and logs them'''
+
+
 def LittleEndianBits(audio):
     pass
+
+
 '''Takes the Most Signidificant Bits of an audio file and logs them'''
+
+
 def BigEndianBits(audio):
     pass
+
+
 '''creates a spectrogram of an audio file'''
+
+
 def spectrogram(audio):
-    pass
+    Fs, Audio = wavfile.read(audio)
+    Audio = Audio[:, 0]
+    first = Audio[:int(Fs*125)]
+    powerSpectrum, frequenciesFound, time, imageAxis = plt.specgram(first, Fs=Fs)
+    plt.show()
+
+
 '''converts non wav files to wav'''
+
+
 def convertToWav(audio):
     pass
+
+
+
 '''decodes SSTV signals from an audio file'''
-def SSTVdecode(audio,mode):
+
+
+def SSTVdecode(audio, mode):
     pass
+
+
+spectrogram("test resources/example.wav")
